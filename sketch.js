19212a8e7b5189193a9452e0d6d4bdfbee84dadd
Riddhiman6042 <88177803+Riddhiman6042@users.noreply.github.com>
2021-08-06 09:08:30 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,34 @@
-function setup() {
-  createCanvas(1900,950);
+var sea,seaImg,edges;
+var ship,shipAni;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipAni = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 }
 
-function draw() 
-{
-  background(255,255,255);
+function setup(){
+  createCanvas(1900,950);
 
-  if(keyIsDown(UP_ARROW))
+// create the background
+  sea = createSprite(550,475,900,950);
+  sea.addAnimation("background",seaImg);
+
+// create the ship
+  ship = createSprite(250,500);
+  ship.addAnimation("Playable_character",shipAni);
+  ship.scale = 0.5;
+}
+
+function draw() {
+  background("white");
+
+  sea.velocityX = -5;
+
+// make the world infinite  
+  if(sea.x < 0)
    {
-     background(255,154,0);
+     sea.x = sea.width / 2;
    }
 
-   if(keyIsDown(DOWN_ARROW))
-    {
-      background(0,154,255);
-    }
-
-    if(keyIsDown(LEFT_ARROW))
-     {
-       background(255,0,154);
-     }
-     if(keyIsDown(RIGHT_ARROW))
-      {
-        background(50,0,255);
-      }
+  drawSprites();
 }
-
-
-
-
